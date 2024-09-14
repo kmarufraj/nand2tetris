0dde8d09fe2a2ffdd5016c960cc159471231daa1
kmarufraj <kmarufraj@gmail.com>
2024-09-14 13:21:08 +0530

    add this to demo codes

--- projects/demo codes/jmp_based_codes.asm
+++ projects/demo codes/jmp_based_codes.asm
@@ -8,7 +8,7 @@
 @RO
 D=M  //  D = RAM[0]
 
-08
+@POSITIVE
 D; JGT  // If R0>0 goto 8
 
 @R1
@@ -16,8 +16,10 @@ M=0  // RAM[1] = 0
 @10
 0; JMP  // end of progress
 
+(POSITIVE)
 @R1
 M=1  // R1=1
 
-@10
+(END)
+@END
 0; JMP
