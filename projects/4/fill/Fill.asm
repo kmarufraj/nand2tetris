9c0ed1827651966bd0b37398ef82c5bf9c74ebb6
kmarufraj <kmarufraj@gmail.com>
2024-09-17 12:20:53 +0530

    add fill to project-4

--- projects/4/fill/Fill.asm
+++ projects/4/fill/Fill.asm
@@ -8,4 +8,46 @@
 // i.e. writes "black" in every pixel. When no key is pressed, 
 // the screen should be cleared.
 
-//// Replace this comment with your code.
+    @8192
+    D=A 
+    @ENDSCREEN
+    M=D
+(BEGIN)
+    @0
+    D=A
+    @i
+    M=D
+(LOOP)
+    // Increment of i
+    @i 
+    M=M+1
+
+    // Restart if needed
+    D=M
+    @ENDSCREEN
+    D=M-D
+    @BEGIN
+    D;JEQ
+
+    @KBD
+    D=M
+    @FILLSCREEN
+    D;JNE
+
+(CLEARSCREEN)  
+    @i
+    D=M
+    @SCREEN
+    A=A+D  
+    M=0
+    @LOOP
+    0;JMP
+
+(FILLSCREEN)
+    @i
+    D=M
+    @SCREEN
+    A=A+D  
+    M=-1
+    @LOOP
+    0;JMP
